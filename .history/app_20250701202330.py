c6c1c96cbc3f73b1ec0e94565f3510dde8e39c2a
Paul R <paulyacha@gmail.com>
2025-07-01 20:23:42 +0200

    working on streamlit app

--- .history/app_20250701202330.py
+++ .history/app_20250701202330.py
@@ -158,7 +158,7 @@ if uploaded_file:
 
     col1, col2 = st.columns(2)
     col1.metric("Max conversions week:", f"{weekly_flow_all_time_result['max_conv_value']} - ({weekly_flow_all_time_result['max_conv_label']})")
-    col2.metric("Min conversions week:", f"{weekly_flow_all_time_result['min_conv_value']} - ({weekly_flow_all_time_result['min_conv_label']})")
+    col2.metric("Min conversions week:", f"{weekly_flow_all_time_result['max_conv_value']} - ({weekly_flow_all_time_result['min_conv_label']})")
 
     st.header("RENEWAL FLOW")
     st.pyplot(fig_renewal_8w)
